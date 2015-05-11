a6f13220424f989facb02391b462e0ab314f76aa
kpardo <kpardo@users.noreply.github.com>
2015-05-10 22:34:28 -0400

    Added notes to self.

--- src/force.cc
+++ src/force.cc
@@ -31,11 +31,41 @@ void Force::calculateforce(Particle_vector &p){
 	int NumParticles = particles.size();
 	
 	//Set acceleration to 0
-	
+  	for (auto &a : p) {
+    		a.ax = 0.;
+		a.ay = 0.;
+    		a.az = 0.;
+	 }
 	//Loop PROPERLY over particles and add to acceleration
 	//in EACH direction.
-	
+	//FOLLOWING IS SEMYOUNG'S CODE
 	/*
+	  double r, dax, day, daz;  // temp variables
+	  for (auto i = p.begin(); i != p.end(); ++i) {
+	    r = 0.;
+	    dax = 0.;
+	    day = 0.;
+	    daz = 0.;
+	    for (auto j = i+1; j != p.end(); ++j) {
+	      r = i->d(*j);
+	      dax = (j->x - i->x) / pow(r, 3);
+	      day = (j->y - i->y) / pow(r, 3);
+	      daz = (j->z - i->z) / pow(r, 3);
+	      // add to p_i and p_j with different signs
+	      i->ax += j->mass * dax;
+	      i->ay += j->mass * day;
+	      i->az += j->mass * daz;
+	      j->ax -= i->mass * dax;
+	      j->ay -= i->mass * day;
+	      j->az -= i->mass * daz;
+	    }
+	    i->ax *= G;
+	    i->ay *= G;
+	    i->az *= G;
+	  }
+	*/
+	
+	/* THE FOLLOWING IS OLD. WILL KEEP FOR NOW.
 	if (NumParticles == 1){
 		float r = findseparation(p1, p2)
 		force = -G*p1.mass*p2.mass/pow(r,3) //Make a separate function for this?
