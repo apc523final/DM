55b410593898fca4fc44f7b52719d6d9d5084431
kpardo <kpardo@users.noreply.github.com>
2015-05-10 22:22:56 -0400

    Fixed some spelling mistakes

--- src/force.cc
+++ src/force.cc
@@ -32,7 +32,7 @@ void Force::main(Particle_vector &particles){
 }
 
 //Function that calculates force
-float calculateforce(Particle_vector &p){
+void Force::calculateforce(Particle_vector &p){
 	if (NumParticles == 1){
 		float r = findseparation(p1, p2)
 		force = -G*p1.mass*p2.mass/pow(r,3) //Make a separate function for this?
@@ -42,7 +42,7 @@ float calculateforce(Particle_vector &p){
 		float r = findseparation(p1, pm)
 		float d = root.uppdercorner()-root.lowercorner()
 		if (d/r < theta){
-			//force = F_g of particel and center of mass of node
+			//force = F_g of particle and center of mass of node
 		}
 		else{
 			//for all child nodes n
@@ -53,7 +53,7 @@ float calculateforce(Particle_vector &p){
 
 }
 //Function that finds the separation between two particles
-float findseparation(Particle p1, Particle p2){
+float find3Dseparation(Particle p1, Particle p2){
 	separation = sqrt((p1.x - p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) + (p1.z-p2.z)*(p1.z-p2.z));
 	return separation;
 }
