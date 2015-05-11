e79b351b4b9cec4bf3b5ecb50d9eb42abd7cb18a
kpardo <kpardo@users.noreply.github.com>
2015-05-11 15:03:03 -0400

    Fixed all the things! - Have not tested yet

--- src/force.cc
+++ src/force.cc
@@ -5,11 +5,11 @@
 #include "force.h"
 
 const int G = 6.67E-11 //Gravitational constant in m^3/kg*s^2
+const float theta = 1 //Parameter for deciding when to calculate using COM or going to children nodes
 
 /*NOTE TO FUTURE KRISTINA: USE: https://github.com/Nbodypp/HOW_final/blob/master/src/force_direct.cc
 NEED TO FIX calculateforce function!
 ALSO: FIX THE DANG HEADER FILE!
-
 */
 
 //Force Constructor
@@ -21,73 +21,61 @@ Force::Force(int gravity/*=1*/)
 Force::~Force() {
 }
 
-void Force::main(Particle_vector &particles){
-	calculateforce(particles)
+void Force::main(Node_vector &nodes, Particle_vector &particles){
+	calculateforce(nodes, particles)
 }
 
 //Function that calculates force
-void Force::calculateforce(Particle_vector &p){
-	//Find total number of particles
-	int NumParticles = particles.size();
+void Force::calculateforce(Node_vector &n, Particle_vector &p){
+	
+	
 	
 	//Set acceleration to 0
   	for (auto &a : p) {
     		a.ax = 0.;
-		a.ay = 0.;
-    		a.az = 0.;
+			a.ay = 0.;
+    		// a.az = 0.;
 	 }
+
 	//Loop PROPERLY over particles and add to acceleration
 	//in EACH direction.
 	//FOLLOWING IS SEMYOUNG'S CODE
-	/*
+	
 	  double r, dax, day, daz;  // temp variables
 	  for (auto i = p.begin(); i != p.end(); ++i) {
 	    r = 0.;
 	    dax = 0.;
 	    day = 0.;
-	    daz = 0.;
-	    for (auto j = i+1; j != p.end(); ++j) {
+	    // daz = 0.;
+	    for (auto j = n.begin(); j != n.end(); ++j) {
 	      r = i->d(*j);
+	      l = j.uppercorner-j.lowercorner
+	      if (l/r<theta){
+	      	continue 
+	      }
+	      else{
+	      	child = n.children
+	      	calculateforce(&child, &p)
+	      	break
+	      }
+	      j->x = j.com[0]
+	      j->y = j.com[1]
+	      j->mass = j.mass
+
 	      dax = (j->x - i->x) / pow(r, 3);
 	      day = (j->y - i->y) / pow(r, 3);
-	      daz = (j->z - i->z) / pow(r, 3);
+	      // daz = (j->z - i->z) / pow(r, 3);
 	      // add to p_i and p_j with different signs
 	      i->ax += j->mass * dax;
 	      i->ay += j->mass * day;
-	      i->az += j->mass * daz;
+	      // i->az += j->mass * daz;
 	      j->ax -= i->mass * dax;
 	      j->ay -= i->mass * day;
-	      j->az -= i->mass * daz;
+	      // j->az -= i->mass * daz;
 	    }
 	    i->ax *= G;
 	    i->ay *= G;
-	    i->az *= G;
+	    // i->az *= G;
 	  }
-	*/
-	
-	/* THE FOLLOWING IS OLD. WILL KEEP FOR NOW.
-	if (NumParticles == 1){
-		float r = findseparation(p1, p2)
-		force = -G*p1.mass*p2.mass/pow(r,3) //Make a separate function for this?
-	}
-	else{
-		Particle pm = root.com;
-		float r = findseparation(p1, pm)
-		float d = root.uppdercorner()-root.lowercorner()
-		if (d/r < theta){
-			//force = F_g of particle and center of mass of node
-		}
-		else{
-			//for all child nodes n
-				//force += node.calculateforce(particle)
-		}
-	}
-	return force;
-	*/
 
 }
-//Function that finds the separation between two particles - DO NOT NEED.
-// float find3Dseparation(Particle p1, Particle p2){
-// 	separation = sqrt((p1.x - p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) + (p1.z-p2.z)*(p1.z-p2.z));
-// 	return separation;
-// }
