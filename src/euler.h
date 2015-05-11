c488abf230a6a44f4ca8462fcffead4acc56c420
kpardo <kpardo@princeton.edu>
2015-05-11 18:00:36 -0400

    added integrator and euler files

---
+++ src/euler.h
@@ -0,0 +1,26 @@
+
+/**
+ * @file euler.h
+ * @brief Forward Euler integrator
+ */
+
+#ifndef EULER_H_
+#define EULER_H_
+
+#include "integrator.h"
+#include "force.h"
+
+/**
+ * Forward Euler integrator.
+ */
+class Euler : public Integrator {
+ public:
+  Euler(double dt, const Force &force);
+  ~Euler();
+  int step(double t, Particles &particles);
+ private:
+  const double dt_;       // timestep
+  const Force &force_;    // Force model
+};
+
+#endif  // EULER_H_
