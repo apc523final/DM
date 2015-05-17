4d4342bd8ac3f5d98f5287afe8d3ab8ad05fa030
Joshua <joshuawallace800@gmail.com>
2015-05-17 16:35:55 -0400

    example code written

--- src/example.main.cc
+++ src/example.main.cc
@@ -1,4 +1,7 @@
 #include <cmath>
+/*This is an example code, showing the basic parts that need to be put 
+  together for the code to work.*/
+
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,14 +18,18 @@
 
 int main()
 {
-  double dt = .5e-4;
+  //Define a time step size
+  double dt = 1.e-3;
   
-  int numx = 5;
-  int numy = 5;
-  double particlemass = 1.0;
-  
+  //Define a Particle_vector, here named particles, in which to store the particles of the simulation
   Particle_vector particles;
 
+  //Add particles to the Particle_vector
+  double particlemass = 1.;
+  particles.push_back(Particle(particlemass,1.,1.));
+  particles.push_back(Particle(particlemass,-1.,-1.));
+
+  //Define the lower and upper corners of the simulation box
   double lowercorner[2];
   double uppercorner[2];
   lowercorner[0]=-5.0;
@@ -30,28 +37,16 @@ int main()
   uppercorner[0]=5.0;
   uppercorner[1]=5.0;
 
-  for(int i=0; i<numx; i++)
-    {
-      for(int j=0; j<numy; j++)
-        {
-          double x = ( ((double)i+.5)/((double)(numx))) * (uppercorner[0] - lowercorner[0]) + lowercorner[0];
-          double y = ( ((double)j+.5)/((double)(numy))) * (uppercorner[1] - lowercorner[1]) + lowercorner[1];
-          particles.push_back(Particle(particlemass,x,y));
-        }
-    }
-
-  lowercorner[0]=-500.0;
-  lowercorner[1]=-500.0;
-  uppercorner[0]=500.0;
-  uppercorner[1]=500.0;
-
-
-
   
+  //Create a Force type, here named force
   Force force;
-  
+
+  //Here choose the integration scheme:
+  //a string named integrator_name is used to store the
+  //choice of integration scheme
   const std::string integrator_name = "rk4";
 
+  //and then an Integrator is set up based on the selected integration scheme
   Integrator *integrator = NULL;
   if (integrator_name.compare("euler") == 0) {
     printf("#Setting up an euler integrator\n");
@@ -74,32 +69,43 @@ int main()
             integrator_name.c_str());
   }
 
-  Node root(lowercorner,uppercorner,ROOT);
-  double t = 0.;
-  int z = 0;
-  for(t=0.; t<dt*160000; t+=dt)
-    {
 
+  //Create a root node
+  Node root(lowercorner,uppercorner,ROOT);
+
+  //Create a variable to track the time
+  double t = 0.;
+
+  //Loop for a chosen amount of time
+  for(t=0.; t<dt*100; t+=dt)
+    {
+      //Put the particles all in their appropriate
+      //place in the tree structure
       for(int i=0; i<particles.size(); i++)
         {
           root.FigureParticle(&particles[i]);
-
         }
-      
+
+      //Define a Node_vector_element_pointer, used in the force calculation
       Node_vector_element_pointer initnodes;
+
+      //Put the root node into this Node_vector_element_pointer
       initnodes.push_back(&root);
+
+      //Step the integrator
       integrator->step(t, particles, initnodes);
-      
 
+      //Print the particle locations and/or energies
+      //The setup below currently just prints to stdout
+      //print_particles prints the x,y,z coordinates of each particle
+      //in a horizontal line as follows:  x1  y1  z1  x2  y2  z2 ...
+      print_particles(particles, std::cout);
+      //prints total energy of all particles
+      printf(" %e ",total_energy(particles));
+      printf("\n");
+
+      //Reset the tree structure
       root.Reset();
-      
-
-       if(z%500 == 0)
-         {
-           print_particles(particles, std::cout);
-           printf("\n");
-         }      
-      z++;
     }
 
   return 0;
